6989ed43913cd4da47840d3d2f405705ecd73189
= <=>
2021-02-22 00:07:51 +0700

    tugas2

--- lib/main.dart
+++ lib/main.dart
@@ -9,24 +9,152 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("MyApp"),
+          backgroundColor: Colors.red,
+        ),
+        body: SingleChildScrollView(
+          child: Container(
+            margin: EdgeInsets.all(2),
+            child: Column(children: [
+              Container(
+                margin: EdgeInsets.all(2),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: OutlineButton(
+                        onPressed: () {},
+                        child: Text(
+                          "BERITA TERBARU",
+                          style: TextStyle(fontSize: 12),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Expanded(
+                      child: OutlineButton(
+                        onPressed: () {},
+                        child: Text(
+                          "PERTANDINGAN HARI INI",
+                          style: TextStyle(fontSize: 10.5),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(),
+              Container(
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.purple[250])),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Image.network(
+                        'https://www.spurs-web.com/static/uploads/2019/07/skysports-diego-costa-atletico-madrid_4644146.jpg'),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Center(
+                        child: Text(
+                          "Costa Mendekat Ke Palmeiras",
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.w500),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.fromLTRB(8, 12, 336, 12),
+                      color: Colors.purple[250],
+                      child: Text(
+                        "Transfer",
+                        style: TextStyle(fontSize: 15),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 2,
+              ),
+              Container(
+                decoration:
+                    BoxDecoration(border: Border.all(color: Colors.black)),
+                margin: EdgeInsets.only(top: 10),
+                child: Row(
+                  children: <Widget>[
+                    Column(children: <Widget>[
+                      Image.network(
+                        "https://images.daznservices.com/di/library/GOAL/c0/68/gerard-pique-barcelona-yellow-card-2020-21_1b5pixs2oir8s1hy0nwfy1tnrz.jpg?t=1931977831&quality=60&w=1200&h=800",
+                        width: 150.0,
+                        height: 120.0,
+                        fit: BoxFit.cover,
+                      ),
+                    ]),
+                    Container(
+                      margin: EdgeInsets.fromLTRB(10, 0, 0, 0),
+                      alignment: Alignment.center,
+                      child: Text(
+                          "Pique Bilang Wasit Untungkan Madrid, Koeman Tapuk Jidat",
+                          maxLines: 2,
+                          style: TextStyle(color: Colors.black, fontSize: 12)),
+                      height: 120,
+                      width: 170,
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.black)),
+                      alignment: Alignment.centerLeft,
+                      child: Text("Barcelona Feb 13, 2021",
+                          style: TextStyle(color: Colors.black)),
+                      height: 30,
+                      width: 407,
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                decoration:
+                    BoxDecoration(border: Border.all(color: Colors.black)),
+                margin: EdgeInsets.only(top: 10),
+                child: Row(
+                  children: <Widget>[
+                    Column(children: <Widget>[
+                      Image.network(
+                        "https://images.daznservices.com/di/library/GOAL/c0/68/gerard-pique-barcelona-yellow-card-2020-21_1b5pixs2oir8s1hy0nwfy1tnrz.jpg?t=1931977831&quality=60&w=1200&h=800",
+                        width: 150.0,
+                        height: 120.0,
+                        fit: BoxFit.cover,
+                      ),
+                    ]),
+                    Container(
+                      margin: EdgeInsets.fromLTRB(10, 0, 0, 0),
+                      alignment: Alignment.center,
+                      child: Text(
+                          "Pique Bilang Wasit Untungkan Madrid, Koeman Tapuk Jidat",
+                          maxLines: 2,
+                          style: TextStyle(color: Colors.black, fontSize: 12)),
+                      height: 120,
+                      width: 170,
+                    ),
+                  ],
+                ),
+              ),
+            ]),
+            //   ],
+            // ),
+          ),
+        ),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
